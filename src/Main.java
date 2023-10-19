ae1b9c2aa5ebf006b1c7cdcedc3f0280b3a125ea
Синюкович Никита Андреевич <mikita.siniukovich@softclub.by>
2023-10-19 17:35:14 +0300

    JA_2_5

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+        String firstName = "", lastName = "", year = "";
         boolean exit = false;
         int number;
         do {
-            System.out.println("""
-                            Menu:
-                    1. Enter your lastname
+            System.out.print("""
+                             Menu:
+                    1. Enter your firstname
                     2. Enter your lastname
                     3. Enter your year of birth
                     4. Display information
-                    0. Exit""");
+                    0. Exit
+                    """);
 
                 System.out.print("Choose the number: ");
                 try {
                     number = Integer.parseInt(scanner.next());
+                    scanner.nextLine();
                     switch (number) {
                         case 1:
-                            System.out.println("Enter your firstname");
+                            System.out.print("Enter your firstname: ");
+                            firstName = scanner.nextLine();
                             break;
                         case 2:
-                            System.out.println("Enter your lastname");
+                            System.out.print("Enter your lastname: ");
+                            lastName = scanner.nextLine();
                             break;
                         case 3:
-                            System.out.println("Enter your year of birth");
+                            System.out.print("Enter your year of birth: ");
+                            year = scanner.nextLine();
                             break;
                         case 4:
-                            System.out.println("FirstName: entered firstname, LastName: entered lastname, " +
-                                    "Year of birth: entered year of birth");
+                            if (firstName.isEmpty() || lastName.isEmpty() || year.isEmpty()) {
+                                System.out.println("To display full information enter your firstname, lastname " +
+                                        "and year of birth");
+                            } else {
+                                System.out.printf("FirstName: %s; lastName: %s; year of birth: %s%n",
+                                        firstName, lastName, year);
+                            }
                             break;
                         case 0:
                             System.out.print("Exit");
