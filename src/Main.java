adf922a0837de56ce0617fe7a6043555fa50081d
Синюкович Никита Андреевич <mikita.siniukovich@softclub.by>
2023-10-19 10:58:28 +0300

    Ja_2_5

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,45 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String number = "";
-
-        while (!number.equals("0")) {
-
+        int number = 0;
+        do {
             System.out.println("""
-                    Choose number of Menu:
+                            Menu:
                     1. Enter your lastname
                     2. Enter your lastname
                     3. Enter your year of birth
                     4. Display information
                     0. Exit""");
-            number = scanner.nextLine();
 
-            switch (number) {
-                case "1":
-                    System.out.println("Enter your firstname");
-                    break;
-                case "2":
-                    System.out.println("Enter your lastname");
-                    break;
-                case "3":
-                    System.out.println("Enter your year of birth");
-                    break;
-                case "4":
-                    System.out.println("FirstName: entered firstname, LastName: entered lastname, " +
-                            "Year of birth: entered year of birth");
-                    break;
-                case "0":
-                    System.out.print("Exit");
-                    break;
-                default:
-                    System.out.println("Number is unavailable");
-            }
-        }
+                System.out.print("Choose the number: ");
+                try {
+                    number = Integer.parseInt(scanner.next());
+                    switch (number) {
+                        case 1:
+                            System.out.println("Enter your firstname");
+                            break;
+                        case 2:
+                            System.out.println("Enter your lastname");
+                            break;
+                        case 3:
+                            System.out.println("Enter your year of birth");
+                            break;
+                        case 4:
+                            System.out.println("FirstName: entered firstname, LastName: entered lastname, " +
+                                    "Year of birth: entered year of birth");
+                            break;
+                        case 0:
+                            System.out.print("Exit");
+                            break;
+                        default:
+                            System.out.println("Number is unavailable");
+                            }
+                    }
+                    catch (NumberFormatException e) {
+                        System.out.print("Wrong Input!");
+                    }
+        } while (number != 0) ;
     }
 }
+
+
